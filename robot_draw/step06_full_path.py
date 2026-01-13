31d74bd3dac34269491ca14c5fe97999b2b8c534
yqh <1779536319@qq.com>
2026-01-13 15:01:06 +0800

    1/13/15/01

--- robot_draw/step06_full_path.py
+++ robot_draw/step06_full_path.py
@@ -199,8 +199,8 @@ if __name__ == "__main__":
     generator = StructuredPathGenerator(INPUT_FILE, OUTPUT_FILE)
 
     # 👉 接口参数
-    DRAW_Z = 120.0  
-    AIR_Z = 140.0   
+    DRAW_Z = 200.0  
+    AIR_Z = 210.0   
     AIR_MOVE_STEP = 0.5 # 采样更密，适应后续运动学要求
     
     generator.generate(DRAW_Z, AIR_Z, AIR_MOVE_STEP)
